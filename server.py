967309101bfcefd4507989ad60a6efdb7d2fa7fd
benasmith1 <81334348+benasmith1@users.noreply.github.com>
2025-04-01 11:07:42 -0600

    before request

--- server.py
+++ server.py
@@ -68,6 +68,14 @@ def submit():
         response.headers.add("Access-Control-Allow-Origin", "*")
         return 
 
+@app.before_request
+def before_request():
+    headers = {'Access-Control-Allow-Origin': '*',
+               'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+               'Access-Control-Allow-Headers': 'Content-Type'}
+    if request.method.lower() == 'options':
+        return jsonify(headers), 200
+    
 def _build_cors_preflight_response():
     response = make_response()
     response.headers.add("Access-Control-Allow-Origin", "*")
